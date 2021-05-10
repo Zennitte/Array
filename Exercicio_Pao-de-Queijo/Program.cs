e386767529fad03ab2575c8c660aacfb191fdb1d
Kauê Camargo <camargokaue92@gmail.com>
2021-05-10 18:03:08 -0300

    Array

---
+++ Exercicio_Pao-de-Queijo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio_Pao_de_Queijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] paoDeQueijo;
+
+            Console.WriteLine("Quantas embalagens de pão de queijo tem na prateleira");
+            int quantidadeEmbalagens = int.Parse(Console.ReadLine());
+
+            paoDeQueijo = new int[quantidadeEmbalagens];
+
+            for (var i = 0; i < quantidadeEmbalagens; i++)
+            {
+                Console.WriteLine($"Digite a quantidade de pão de queijo da {(i+1)}ª embalagem");
+                paoDeQueijo[i] = int.Parse(Console.ReadLine());
+            }
+
+            int c = 1;
+            foreach (var item in paoDeQueijo)
+            {
+                Console.WriteLine($"A {c}ª emabalgem tem: {item} pães de queijo");
+            }
+        }
+    }
+}
---
+++ Exercicio_Produtos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercicio_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome;
+            int[] quantidade;
+            double[] valor;
+
+
+            Console.WriteLine("Quantos produtos você quer cadastrar");
+            int quantidadeProdutos = int.Parse(Console.ReadLine());
+
+            nome = new string[quantidadeProdutos];
+            quantidade = new int[quantidadeProdutos];
+            valor = new double[quantidadeProdutos];
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Qual o nome do {(i+1)}° produto?");
+                nome[i] = Console.ReadLine();
+            }
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Qual a quantidade do {(i+1)}° produto?");
+                quantidade[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Qual o valor do {(i+1)}° produto?");
+                valor[i] = double.Parse(Console.ReadLine());
+            }
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"| Nome      | Quantidade        | Valor         |");
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($@"| {nome[i]}         |        {quantidade[i]}          |       {valor[i]}       |");                                                                               
+            }
+
+
+
+            
+        }
+    }
+}
